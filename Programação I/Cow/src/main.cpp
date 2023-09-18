6b2dab7c900c046eb9fd652c85d31998b7ed4f42
RachelLyns <127237952+RachelLyns@users.noreply.github.com>
2023-09-18 16:53:17 -0300

    Att

--- Programação I/Cow/src/main.cpp
+++ Programação I/Cow/src/main.cpp
@@ -2,11 +2,51 @@
 #include <string>
 #include <cstring>
 
+
+
+
+
+
+int contador_space(int a, int w, const char* vChar, int size){
+    int space = 0;
+    bool left = true;
+    bool right = true;
+     for (int b = 0; b < w; b++, a++) {
+            if((vChar[(a+b)] == ' ')&&(left == true)){
+                space++;
+            }else{
+                left = false;
+            }
+            if(((vChar[(a+w-b-1)] == ' ')||(a+w-b > size))&&(right == true)){
+                space++;
+            }else{
+                right = false;
+            }
+        }
+    return space;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main() {
     std::string text = "aaa aaa aaa aaa aaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa          aaaaaaaa";
     int size = text.size();
     char vChar[size-1];
     int w = 15;
+    int space;
 
     std::cout << size << " Letras \n\n";
     strcpy(vChar, text.c_str());
@@ -18,15 +58,25 @@ int main() {
 
 
     for (int a = 0; a < size-1; a) {
-        std::cout << "| ";
+        std::cout << '| ';
+
+        space = contador_space(a, w, vChar, size);
+
+        for (int c = 1; c < space; c++){
+            std::cout << ' ';
+        }
+
         for (int b = 0; b < w; b++, a++) {
             if(a < size){
                 std::cout << vChar[a];
-            }else{
-                std::cout << " ";
             }
         }
-        std::cout << " |\n";
+
+        for (int c = 1; c < space; c++){
+            std::cout << ' ';
+        }
+
+        std::cout << ' |\n';
     }
     
 
@@ -43,8 +93,6 @@ int main() {
     return 0;
 
 }
-
-
 
 
 
